a80086dbb6006620a854f08b62e3e57623d91730
Gabriel Montano <gabo88lope@users.noreply.github.com>
2018-03-18 01:42:39 -0600

    Eliminacion de USER CONTROL, y ver otras opciones

--- HonDB/HonDBSec.vb
+++ HonDB/HonDBSec.vb
@@ -1,9 +1,7 @@
 ﻿Public Class HonDBSec
     Private Sub HonDBSec_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
-        Dim np As NuevoPrestamo
 
-        np.Visible = True
 
     End Sub
 End Class
