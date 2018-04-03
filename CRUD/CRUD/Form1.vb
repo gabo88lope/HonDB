7eb48f5cc1d25e2cc864d54869fec3b499864187
lordBarr <36232017+lordBarr@users.noreply.github.com>
2018-04-03 11:29:24 -0600

    Colocación de Background de Ventana de Inicio.

---
+++ CRUD/CRUD/Form1.vb
@@ -0,0 +1,7 @@
+﻿Public Class PortadaInicio
+    Private Sub Inicio_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Me.FormBorderStyle = Windows.Forms.FormBorderStyle.None
+        Me.WindowState = FormWindowState.Maximized
+    End Sub
+
+End Class
