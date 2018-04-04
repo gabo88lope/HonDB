f37b2e13d6e56af35bcc7c7f7967af90ece3db80
Gabriel Montano <gabo88lope@users.noreply.github.com>
2018-04-03 21:42:35 -0600

    Revert "Colocación de Background de Ventana de Inicio."
    This reverts commit 7eb48f5cc1d25e2cc864d54869fec3b499864187.

--- CRUD/CRUD/Form1.vb
+++
@@ -1,7 +0,0 @@
-﻿Public Class PortadaInicio
-    Private Sub Inicio_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        Me.FormBorderStyle = Windows.Forms.FormBorderStyle.None
-        Me.WindowState = FormWindowState.Maximized
-    End Sub
-
-End Class
