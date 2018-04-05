a900af9d2965cf93745f15a1890645c06aeb1e0c
Gabriel Montano <gabo88lope@users.noreply.github.com>
2018-04-05 01:47:29 -0600

    Componer Ingreso en Login y hacer validacion

--- HonDB/Login.vb
+++ HonDB/Login.vb
@@ -12,26 +12,48 @@ Public Class Login
     Private Sub UsernameText_KeyPress(sender As Object, e As KeyPressEventArgs) Handles UsernameText.KeyPress
 
         If e.KeyChar = ChrW(Keys.Enter) Then
-            passText.Text = ""
-            passText.Select()
+            PassText.Text = ""
+            PassText.Select()
         End If
 
     End Sub
 
     Private Sub BotonIngresar_Click(sender As Object, e As EventArgs) Handles BotonIngresar.Click
-        conexion = New MySqlConnection
+        'conexion = New MySqlConnection
 
         Try
-            conexion.ConnectionString = "Server = localhost; Uid= root; Pud = leo12345; DataBase = biblioteca"
-            conexion.Open()
-            MsgBox("Conexíon establecida")
-            ''HonDBPrin.Show()
-            ''HonDBPrin.Focus()
-            ''Me.Close()
+            'conexion.ConnectionString = "Server = localhost; Uid= root; Pud = leo12345; DataBase = biblioteca"
+            'conexion.Open()
+            'MsgBox("Conexíon establecida")
+            HonDBPrin.Show()
+            HonDBPrin.Focus()
+            Me.Close()
         Catch ex As Exception
             MsgBox("Conexíon fallida")
         End Try
 
 
     End Sub
+
+    Private Sub PassText_Click(sender As Object, e As EventArgs) Handles PassText.Click
+        If PassText.Text.Equals("Contraseña") Then
+            PassText.ResetText()
+        End If
+
+        If UsernameText.Text Is Nothing Or UsernameText.Text.Equals("") Then
+            UsernameText.Text = "Username"
+        End If
+
+    End Sub
+
+    Private Sub UsernameText_Click(sender As Object, e As EventArgs) Handles UsernameText.Click
+        If UsernameText.Text.Equals("Username") Then
+            UsernameText.ResetText()
+        End If
+
+        If PassText.Text Is Nothing Or PassText.Text.Equals("") Then
+            PassText.Text = "Contraseña"
+        End If
+    End Sub
+
 End Class
