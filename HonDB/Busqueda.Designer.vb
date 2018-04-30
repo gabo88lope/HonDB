237aaaa10bc9a0c74fb4f1235c13dbadfc0788f0
nel.ubeda <nel.ubeda@gmail.com>
2018-04-30 15:39:37 -0600

    Busqueda

---
+++ HonDB/Busqueda.Designer.vb
@@ -0,0 +1,225 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Busqueda
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.ComboBox4 = New System.Windows.Forms.ComboBox()
+        Me.ComboBox3 = New System.Windows.Forms.ComboBox()
+        Me.ComboBox2 = New System.Windows.Forms.ComboBox()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.CheckBox1 = New System.Windows.Forms.CheckBox()
+        Me.BotonBuscarLibro = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.BotonAgregarBusqueda = New System.Windows.Forms.Button()
+        Me.BotonCancelarBusqueda = New System.Windows.Forms.Button()
+        Me.Panel1.SuspendLayout()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Panel1
+        '
+        Me.Panel1.BackColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(29, Byte), Integer), CType(CType(54, Byte), Integer))
+        Me.Panel1.Controls.Add(Me.Label4)
+        Me.Panel1.Controls.Add(Me.Label3)
+        Me.Panel1.Controls.Add(Me.Label2)
+        Me.Panel1.Controls.Add(Me.Label1)
+        Me.Panel1.Controls.Add(Me.ComboBox4)
+        Me.Panel1.Controls.Add(Me.ComboBox3)
+        Me.Panel1.Controls.Add(Me.ComboBox2)
+        Me.Panel1.Controls.Add(Me.ComboBox1)
+        Me.Panel1.Controls.Add(Me.CheckBox1)
+        Me.Panel1.Controls.Add(Me.BotonBuscarLibro)
+        Me.Panel1.Controls.Add(Me.TextBox1)
+        Me.Panel1.Location = New System.Drawing.Point(-8, -3)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(816, 110)
+        Me.Panel1.TabIndex = 0
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.ForeColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.Label4.Location = New System.Drawing.Point(371, 70)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(126, 13)
+        Me.Label4.TabIndex = 10
+        Me.Label4.Text = "Temática del Documento"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.ForeColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.Label3.Location = New System.Drawing.Point(378, 15)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(119, 13)
+        Me.Label3.TabIndex = 9
+        Me.Label3.Text = "Nombre del Documento"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.ForeColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.Label2.Location = New System.Drawing.Point(586, 70)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(110, 13)
+        Me.Label2.TabIndex = 8
+        Me.Label2.Text = "Fecha de Publicación"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.Label1.Location = New System.Drawing.Point(627, 15)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(32, 13)
+        Me.Label1.TabIndex = 7
+        Me.Label1.Text = "Autor"
+        '
+        'ComboBox4
+        '
+        Me.ComboBox4.FormattingEnabled = True
+        Me.ComboBox4.Location = New System.Drawing.Point(351, 86)
+        Me.ComboBox4.Name = "ComboBox4"
+        Me.ComboBox4.Size = New System.Drawing.Size(174, 21)
+        Me.ComboBox4.TabIndex = 6
+        '
+        'ComboBox3
+        '
+        Me.ComboBox3.FormattingEnabled = True
+        Me.ComboBox3.Location = New System.Drawing.Point(351, 31)
+        Me.ComboBox3.Name = "ComboBox3"
+        Me.ComboBox3.Size = New System.Drawing.Size(174, 21)
+        Me.ComboBox3.TabIndex = 5
+        '
+        'ComboBox2
+        '
+        Me.ComboBox2.FormattingEnabled = True
+        Me.ComboBox2.Location = New System.Drawing.Point(556, 86)
+        Me.ComboBox2.Name = "ComboBox2"
+        Me.ComboBox2.Size = New System.Drawing.Size(174, 21)
+        Me.ComboBox2.TabIndex = 4
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Location = New System.Drawing.Point(556, 31)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(174, 21)
+        Me.ComboBox1.TabIndex = 3
+        '
+        'CheckBox1
+        '
+        Me.CheckBox1.AutoSize = True
+        Me.CheckBox1.ForeColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.CheckBox1.Location = New System.Drawing.Point(148, 15)
+        Me.CheckBox1.Name = "CheckBox1"
+        Me.CheckBox1.Size = New System.Drawing.Size(125, 17)
+        Me.CheckBox1.TabIndex = 2
+        Me.CheckBox1.Text = "Búsqueda Avanzada"
+        Me.CheckBox1.UseVisualStyleBackColor = True
+        '
+        'BotonBuscarLibro
+        '
+        Me.BotonBuscarLibro.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.BotonBuscarLibro.Location = New System.Drawing.Point(207, 48)
+        Me.BotonBuscarLibro.Name = "BotonBuscarLibro"
+        Me.BotonBuscarLibro.Size = New System.Drawing.Size(75, 23)
+        Me.BotonBuscarLibro.TabIndex = 1
+        Me.BotonBuscarLibro.Text = "Buscar"
+        Me.BotonBuscarLibro.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(32, 48)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(150, 20)
+        Me.TextBox1.TabIndex = 0
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.BackgroundColor = System.Drawing.SystemColors.ActiveCaption
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Location = New System.Drawing.Point(-5, 113)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.Size = New System.Drawing.Size(810, 291)
+        Me.DataGridView1.TabIndex = 1
+        '
+        'BotonAgregarBusqueda
+        '
+        Me.BotonAgregarBusqueda.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.BotonAgregarBusqueda.Location = New System.Drawing.Point(713, 410)
+        Me.BotonAgregarBusqueda.Name = "BotonAgregarBusqueda"
+        Me.BotonAgregarBusqueda.Size = New System.Drawing.Size(75, 38)
+        Me.BotonAgregarBusqueda.TabIndex = 2
+        Me.BotonAgregarBusqueda.Text = "Agregar"
+        Me.BotonAgregarBusqueda.UseVisualStyleBackColor = True
+        '
+        'BotonCancelarBusqueda
+        '
+        Me.BotonCancelarBusqueda.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.BotonCancelarBusqueda.Location = New System.Drawing.Point(12, 413)
+        Me.BotonCancelarBusqueda.Name = "BotonCancelarBusqueda"
+        Me.BotonCancelarBusqueda.Size = New System.Drawing.Size(75, 35)
+        Me.BotonCancelarBusqueda.TabIndex = 3
+        Me.BotonCancelarBusqueda.Text = "Cancelar"
+        Me.BotonCancelarBusqueda.UseVisualStyleBackColor = True
+        '
+        'Busqueda
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.BotonCancelarBusqueda)
+        Me.Controls.Add(Me.BotonAgregarBusqueda)
+        Me.Controls.Add(Me.DataGridView1)
+        Me.Controls.Add(Me.Panel1)
+        Me.Name = "Busqueda"
+        Me.Text = "Búsqueda de Libros"
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel1.PerformLayout()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents ComboBox4 As ComboBox
+    Friend WithEvents ComboBox3 As ComboBox
+    Friend WithEvents ComboBox2 As ComboBox
+    Friend WithEvents ComboBox1 As ComboBox
+    Friend WithEvents CheckBox1 As CheckBox
+    Friend WithEvents BotonBuscarLibro As Button
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents BotonAgregarBusqueda As Button
+    Friend WithEvents BotonCancelarBusqueda As Button
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label1 As Label
+End Class
---
+++ HonDB/Busqueda.vb
@@ -0,0 +1,102 @@
+﻿Imports System.Data.OleDb
+Imports MySql.Data.MySqlClient
+
+Public Class Busqueda
+    Dim constr As String = "Server=localhost;user id=root;Password=12345678;database = hondb"
+    Dim con As New MySqlConnection(constr)
+
+    Private Sub CheckBox1_CheckedChanged(sender As Object, e As EventArgs) Handles CheckBox1.CheckedChanged
+
+        If CheckBox1.Checked = True Then
+            ComboBox1.Visible = True
+        Else
+            ComboBox1.Visible = False
+        End If
+
+        If CheckBox1.Checked = True Then
+            ComboBox2.Visible = True
+        Else
+            ComboBox2.Visible = False
+        End If
+
+        If CheckBox1.Checked = True Then
+            ComboBox3.Visible = True
+        Else
+            ComboBox3.Visible = False
+        End If
+
+        If CheckBox1.Checked = True Then
+            ComboBox4.Visible = True
+        Else
+            ComboBox4.Visible = False
+        End If
+
+        If CheckBox1.Checked = True Then
+            Label1.Visible = True
+        Else
+            Label1.Visible = False
+        End If
+
+        If CheckBox1.Checked = True Then
+            Label2.Visible = True
+        Else
+            Label2.Visible = False
+        End If
+
+        If CheckBox1.Checked = True Then
+            Label3.Visible = True
+        Else
+            Label3.Visible = False
+        End If
+
+        If CheckBox1.Checked = True Then
+            Label4.Visible = True
+        Else
+            Label4.Visible = False
+        End If
+
+
+        If CheckBox1.Checked = True Then
+            ComboBox1.Enabled = True
+            ComboBox2.Enabled = True
+            ComboBox3.Enabled = True
+            ComboBox4.Enabled = True
+
+        Else
+            ComboBox1.Enabled = False
+            ComboBox2.Enabled = False
+            ComboBox3.Enabled = False
+            ComboBox4.Enabled = False
+        End If
+    End Sub
+
+
+    Private Sub DataGridView1_CellContentClick(sender As Object, e As DataGridViewCellEventArgs) Handles DataGridView1.CellContentClick
+
+    End Sub
+
+    Private Sub BotonBuscarLibro_Click(sender As Object, e As EventArgs) Handles BotonBuscarLibro.Click
+
+    End Sub
+
+    Private Sub ComboBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ComboBox1.SelectedIndexChanged
+        LlenarTabla(DataGridView1, "SELECT * FROM usuario where sexo = '" & ComboBox1.SelectedItem & "'", "usuario")
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        PopulateCombobox(ComboBox1, "autor", "nombre")
+        PopulateCombobox(ComboBox2, "libro", "fechapublicaion")
+        PopulateCombobox(ComboBox3, "libro", "titulo")
+        PopulateCombobox(ComboBox4, "tipodocumento", "nombre")
+
+        LlenarTabla(DataGridView1, "SELECT * FROM usuario", "usuario")
+    End Sub
+
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
+        LlenarTabla(DataGridView1, "SELECT * FROM usuario where nombre like'" & TextBox1.Text & "%'", "usuario")
+    End Sub
+
+    Private Sub Busqueda_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+End Class
