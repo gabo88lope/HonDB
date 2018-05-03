d6b424cac45f1427611f8bab0c178cd2093d9ffb
Gabriel Montano <gabo88lope@users.noreply.github.com>
2018-05-02 23:45:21 -0600

    Agregar evento CheckedChanged

--- HonDB/Ventana_Registro.vb
+++ HonDB/Ventana_Registro.vb
@@ -129,6 +129,27 @@ Public Class Ventana_Registro
             CantP.Text = DatosGrid.Item(11, S).Value()
             CBBN.SelectedItem = DatosGrid.Item(12, S).Value()
             CBBA.SelectedItem = DatosGrid.Item(13, S).Value()
+        End If
+    End Sub
+
+    Private Sub CBEdit_CheckedChanged(sender As Object, e As EventArgs) Handles CBEdit.CheckedChanged
+        If CBEdit.Checked = True Then
+            Dim S As Integer
+            S = DatosGrid.CurrentRow.Index
+            ID.Text = DatosGrid.Item(0, S).Value()
+            NUsuario.Text = DatosGrid.Item(1, S).Value()
+            AUsuario.Text = DatosGrid.Item(2, S).Value()
+            IDUsuario.Text = DatosGrid.Item(3, S).Value()
+            Pais.Text = DatosGrid.Item(4, S).Value()
+            Ciudad.Text = DatosGrid.Item(5, S).Value()
+            Nacionalidad.Text = DatosGrid.Item(6, S).Value()
+            LBPrestamos.Text = DatosGrid.Item(7, S).Value()
+            CBEstado.Text = DatosGrid.Item(8, S).Value()
+            FP.Text = DatosGrid.Item(9, S).Value()
+            FD.Text = DatosGrid.Item(10, S).Value()
+            CantP.Text = DatosGrid.Item(11, S).Value()
+            CBBN.SelectedItem = DatosGrid.Item(12, S).Value()
+            CBBA.SelectedItem = DatosGrid.Item(13, S).Value()
         ElseIf CBEdit.Checked = False Then
             NUsuario.Clear()
             AUsuario.Clear()
@@ -145,6 +166,4 @@ Public Class Ventana_Registro
             FD.ResetText()
         End If
     End Sub
-
-
 End Class
