0081ac54f23e68623ddef88324a9443af1e52964
Gabriel Montano <gabo88lope@users.noreply.github.com>
2018-05-05 06:32:43 -0600

    Busqueda

--- HonDB/Busqueda.Designer.vb
+++ HonDB/Busqueda.Designer.vb
@@ -26,13 +26,7 @@ Partial Class Busqueda
         Me.Panel1 = New System.Windows.Forms.Panel()
         Me.TBI = New System.Windows.Forms.TextBox()
         Me.Label5 = New System.Windows.Forms.Label()
-        Me.Label4 = New System.Windows.Forms.Label()
-        Me.Label3 = New System.Windows.Forms.Label()
-        Me.Label2 = New System.Windows.Forms.Label()
         Me.Label1 = New System.Windows.Forms.Label()
-        Me.ComboBox4 = New System.Windows.Forms.ComboBox()
-        Me.ComboBox3 = New System.Windows.Forms.ComboBox()
-        Me.ComboBox2 = New System.Windows.Forms.ComboBox()
         Me.ComboBox1 = New System.Windows.Forms.ComboBox()
         Me.CheckBox1 = New System.Windows.Forms.CheckBox()
         Me.BotonLimpiarBusquedaLibro = New System.Windows.Forms.Button()
@@ -49,13 +43,7 @@ Partial Class Busqueda
         Me.Panel1.BackColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(29, Byte), Integer), CType(CType(54, Byte), Integer))
         Me.Panel1.Controls.Add(Me.TBI)
         Me.Panel1.Controls.Add(Me.Label5)
-        Me.Panel1.Controls.Add(Me.Label4)
-        Me.Panel1.Controls.Add(Me.Label3)
-        Me.Panel1.Controls.Add(Me.Label2)
         Me.Panel1.Controls.Add(Me.Label1)
-        Me.Panel1.Controls.Add(Me.ComboBox4)
-        Me.Panel1.Controls.Add(Me.ComboBox3)
-        Me.Panel1.Controls.Add(Me.ComboBox2)
         Me.Panel1.Controls.Add(Me.ComboBox1)
         Me.Panel1.Controls.Add(Me.CheckBox1)
         Me.Panel1.Controls.Add(Me.BotonLimpiarBusquedaLibro)
@@ -84,85 +72,23 @@ Partial Class Busqueda
         Me.Label5.TabIndex = 12
         Me.Label5.Text = "Búsqueda de Libros"
         '
-        'Label4
-        '
-        Me.Label4.AutoSize = True
-        Me.Label4.ForeColor = System.Drawing.SystemColors.ButtonHighlight
-        Me.Label4.Location = New System.Drawing.Point(439, 92)
-        Me.Label4.Name = "Label4"
-        Me.Label4.Size = New System.Drawing.Size(126, 13)
-        Me.Label4.TabIndex = 10
-        Me.Label4.Text = "Temática del Documento"
-        Me.Label4.Visible = False
-        '
-        'Label3
-        '
-        Me.Label3.AutoSize = True
-        Me.Label3.ForeColor = System.Drawing.SystemColors.ButtonHighlight
-        Me.Label3.Location = New System.Drawing.Point(446, 46)
-        Me.Label3.Name = "Label3"
-        Me.Label3.Size = New System.Drawing.Size(119, 13)
-        Me.Label3.TabIndex = 9
-        Me.Label3.Text = "Nombre del Documento"
-        Me.Label3.Visible = False
-        '
-        'Label2
-        '
-        Me.Label2.AutoSize = True
-        Me.Label2.ForeColor = System.Drawing.SystemColors.ButtonHighlight
-        Me.Label2.Location = New System.Drawing.Point(688, 92)
-        Me.Label2.Name = "Label2"
-        Me.Label2.Size = New System.Drawing.Size(52, 13)
-        Me.Label2.TabIndex = 8
-        Me.Label2.Text = "Categoria"
-        Me.Label2.Visible = False
-        '
         'Label1
         '
         Me.Label1.AutoSize = True
         Me.Label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight
-        Me.Label1.Location = New System.Drawing.Point(688, 46)
+        Me.Label1.Location = New System.Drawing.Point(619, 47)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(32, 13)
+        Me.Label1.Size = New System.Drawing.Size(53, 13)
         Me.Label1.TabIndex = 7
-        Me.Label1.Text = "Autor"
+        Me.Label1.Text = "Filtrar por:"
         Me.Label1.Visible = False
-        '
-        'ComboBox4
-        '
-        Me.ComboBox4.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
-        Me.ComboBox4.FormattingEnabled = True
-        Me.ComboBox4.Location = New System.Drawing.Point(422, 106)
-        Me.ComboBox4.Name = "ComboBox4"
-        Me.ComboBox4.Size = New System.Drawing.Size(174, 21)
-        Me.ComboBox4.TabIndex = 6
-        Me.ComboBox4.Visible = False
-        '
-        'ComboBox3
-        '
-        Me.ComboBox3.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
-        Me.ComboBox3.FormattingEnabled = True
-        Me.ComboBox3.Location = New System.Drawing.Point(422, 62)
-        Me.ComboBox3.Name = "ComboBox3"
-        Me.ComboBox3.Size = New System.Drawing.Size(174, 21)
-        Me.ComboBox3.TabIndex = 5
-        Me.ComboBox3.Visible = False
-        '
-        'ComboBox2
-        '
-        Me.ComboBox2.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
-        Me.ComboBox2.FormattingEnabled = True
-        Me.ComboBox2.Location = New System.Drawing.Point(622, 106)
-        Me.ComboBox2.Name = "ComboBox2"
-        Me.ComboBox2.Size = New System.Drawing.Size(174, 21)
-        Me.ComboBox2.TabIndex = 4
-        Me.ComboBox2.Visible = False
         '
         'ComboBox1
         '
         Me.ComboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
         Me.ComboBox1.FormattingEnabled = True
-        Me.ComboBox1.Location = New System.Drawing.Point(622, 62)
+        Me.ComboBox1.Items.AddRange(New Object() {"Nombre del autor", "Nombre del documento", "Tematica del docuemento", "Categoría"})
+        Me.ComboBox1.Location = New System.Drawing.Point(622, 63)
         Me.ComboBox1.Name = "ComboBox1"
         Me.ComboBox1.Size = New System.Drawing.Size(174, 21)
         Me.ComboBox1.TabIndex = 3
@@ -182,7 +108,7 @@ Partial Class Busqueda
         'BotonLimpiarBusquedaLibro
         '
         Me.BotonLimpiarBusquedaLibro.Cursor = System.Windows.Forms.Cursors.Hand
-        Me.BotonLimpiarBusquedaLibro.Location = New System.Drawing.Point(323, 62)
+        Me.BotonLimpiarBusquedaLibro.Location = New System.Drawing.Point(481, 63)
         Me.BotonLimpiarBusquedaLibro.Name = "BotonLimpiarBusquedaLibro"
         Me.BotonLimpiarBusquedaLibro.Size = New System.Drawing.Size(75, 21)
         Me.BotonLimpiarBusquedaLibro.TabIndex = 1
@@ -191,9 +117,9 @@ Partial Class Busqueda
         '
         'TextBox1
         '
-        Me.TextBox1.Location = New System.Drawing.Point(20, 62)
+        Me.TextBox1.Location = New System.Drawing.Point(20, 64)
         Me.TextBox1.Name = "TextBox1"
-        Me.TextBox1.Size = New System.Drawing.Size(297, 20)
+        Me.TextBox1.Size = New System.Drawing.Size(455, 20)
         Me.TextBox1.TabIndex = 0
         '
         'DataGridView1
@@ -252,9 +178,6 @@ Partial Class Busqueda
     End Sub
 
     Friend WithEvents Panel1 As Panel
-    Friend WithEvents ComboBox4 As ComboBox
-    Friend WithEvents ComboBox3 As ComboBox
-    Friend WithEvents ComboBox2 As ComboBox
     Friend WithEvents ComboBox1 As ComboBox
     Friend WithEvents CheckBox1 As CheckBox
     Friend WithEvents BotonLimpiarBusquedaLibro As Button
@@ -262,9 +185,6 @@ Partial Class Busqueda
     Friend WithEvents DataGridView1 As DataGridView
     Friend WithEvents BotonAgregarBusqueda As Button
     Friend WithEvents BotonCancelarBusqueda As Button
-    Friend WithEvents Label4 As Label
-    Friend WithEvents Label3 As Label
-    Friend WithEvents Label2 As Label
     Friend WithEvents Label1 As Label
     Friend WithEvents Label5 As Label
     Friend WithEvents TBI As TextBox
--- HonDB/Busqueda.vb
+++ HonDB/Busqueda.vb
@@ -18,88 +18,21 @@ Public Class Busqueda
         End If
 
         If CheckBox1.Checked = True Then
-            ComboBox2.Visible = True
-        Else
-            ComboBox2.Visible = False
-        End If
-
-        If CheckBox1.Checked = True Then
-            ComboBox3.Visible = True
-        Else
-            ComboBox3.Visible = False
-        End If
-
-        If CheckBox1.Checked = True Then
-            ComboBox4.Visible = True
-        Else
-            ComboBox4.Visible = False
-        End If
-
-        If CheckBox1.Checked = True Then
             Label1.Visible = True
         Else
             Label1.Visible = False
         End If
 
         If CheckBox1.Checked = True Then
-            Label2.Visible = True
-        Else
-            Label2.Visible = False
-        End If
-
-        If CheckBox1.Checked = True Then
-            Label3.Visible = True
-        Else
-            Label3.Visible = False
-        End If
-
-        If CheckBox1.Checked = True Then
-            Label4.Visible = True
-        Else
-            Label4.Visible = False
-        End If
-
-
-        If CheckBox1.Checked = True Then
             ComboBox1.Enabled = True
-            ComboBox2.Enabled = True
-            ComboBox3.Enabled = True
-            ComboBox4.Enabled = True
 
         Else
             ComboBox1.Enabled = False
-            ComboBox2.Enabled = False
-            ComboBox3.Enabled = False
-            ComboBox4.Enabled = False
         End If
     End Sub
 
 
-    Private Sub DataGridView1_CellContentClick(sender As Object, e As DataGridViewCellEventArgs) Handles DataGridView1.CellContentClick
-
-    End Sub
-
-    Private Sub BotonBuscarLibro_Click(sender As Object, e As EventArgs) Handles BotonLimpiarBusquedaLibro.Click
-
-    End Sub
-
     Private Sub ComboBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ComboBox1.SelectedIndexChanged
-        LlenarTabla(DataGridView1, "SELECT l.idlibro as Código, l.isbn as ISBN,(SELECT concat_ws(' ',a.nombre,concat(' ', a.apellido)) FROM 
-        autor a INNER JOIN detallelibro d ON a.idautor = d.idautor
-        WHERE d.idlibro = l.idlibro) as Autor, l.depositolegal as DepositoLegal, l.titulo as Titulo,
-        YEAR(l.fechapublicacion) as FechaPublicacion, l.edicion as Edicion, l.descripcion as Descripcion,
-        l.paginas as Paginas, l.numejemplares as Ejemplares, u.ciudad as Ciudad, u.pais as Pais, t.nombre as TipoDocumento, g.nombre as CategoriaGeneral, e.nombre as CategoriaEspecial
-        FROM libro l INNER JOIN ubicacion u ON l.idubicacion = u.idubicacion  INNER JOIN tipodocumento t ON l.idtipodocumento = t.idtipodocumento INNER JOIN categoriageneral
-        g  ON l.codigogeneral = g.codigogeneral INNER JOIN categoriaespecial e ON l.codigoespecial = e.codigoespecial INNER
-        JOIN detallelibro d ON l.idlibro = d.idlibro;", "libro")
-    End Sub
-
-    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        PopulateCombobox(ComboBox1, "autor", "nombre")
-        PopulateCombobox(ComboBox2, "categoriageneral", "nombre")
-        'PopulateCombobox(ComboBox3, "libro", "titulo")
-        PopulateCombobox(ComboBox4, "tipodocumento", "nombre")
-
         LlenarTabla(DataGridView1, "SELECT l.idlibro as Código, l.isbn as ISBN,(SELECT concat_ws(' ',a.nombre,concat(' ', a.apellido)) FROM 
         autor a INNER JOIN detallelibro d ON a.idautor = d.idautor
         WHERE d.idlibro = l.idlibro) as Autor, l.depositolegal as DepositoLegal, l.titulo as Titulo,
@@ -121,11 +54,17 @@ Public Class Busqueda
         FROM libro l INNER JOIN ubicacion u ON l.idubicacion = u.idubicacion  INNER JOIN tipodocumento t ON l.idtipodocumento = t.idtipodocumento INNER JOIN categoriageneral
         g  ON l.codigogeneral = g.codigogeneral INNER JOIN categoriaespecial e ON l.codigoespecial = e.codigoespecial INNER
         JOIN detallelibro d ON l.idlibro = d.idlibro GROUP BY l.idlibro HAVING l.titulo LIKE '" & TextBox1.Text & "%';", "libro")
-
     End Sub
 
     Private Sub Busqueda_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-
+        LlenarTabla(DataGridView1, "SELECT l.idlibro as Código, l.isbn as ISBN,(SELECT concat_ws(' ',a.nombre,concat(' ', a.apellido)) FROM 
+        autor a INNER JOIN detallelibro d ON a.idautor = d.idautor
+        WHERE d.idlibro = l.idlibro) as Autor, l.depositolegal as DepositoLegal, l.titulo as Titulo,
+        YEAR(l.fechapublicacion) as FechaPublicacion, l.edicion as Edicion, l.descripcion as Descripcion,
+        l.paginas as Paginas, l.numejemplares as Ejemplares, u.ciudad as Ciudad, u.pais as Pais, t.nombre as TipoDocumento, g.nombre as CategoriaGeneral, e.nombre as CategoriaEspecial
+        FROM libro l INNER JOIN ubicacion u ON l.idubicacion = u.idubicacion  INNER JOIN tipodocumento t ON l.idtipodocumento = t.idtipodocumento INNER JOIN categoriageneral
+        g  ON l.codigogeneral = g.codigogeneral INNER JOIN categoriaespecial e ON l.codigoespecial = e.codigoespecial INNER
+        JOIN detallelibro d ON l.idlibro = d.idlibro;", "libro")
     End Sub
 
     Private Sub DataGridView1_CellClick(sender As Object, e As DataGridViewCellEventArgs) Handles DataGridView1.CellClick
@@ -146,9 +85,5 @@ Public Class Busqueda
 
 
     End Sub
-
-
-
-
 
 End Class
